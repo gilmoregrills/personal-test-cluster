6e80caa56d922177aa51a625548f4f68c1e63290
gilmoregrills <robinfarrowyonge@gmail.com>
2018-11-29 17:28:07 +0000

    fix gcp backend

--- terraform/google.tf
+++ terraform/google.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("account.json")}"
+  credentials = "${file("../service_account.json")}"
   project     = "kube-personal"
   region      = "europe-west2"
 }
